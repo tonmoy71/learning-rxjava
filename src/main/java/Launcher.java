80ddfb4149da75546bda5ef587dc7a1e05800a9b
Fahim Masud Choudhury <fahim.choudhury@sekai-lab-bd.com>
2017-07-24 15:53:22 +0600

    Initial commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,32 @@
+import io.reactivex.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Fahim on 24-Jul-17.
+ */
+
+public class Launcher {
+    public static void main(String[] args) {
+        Observable<String> myStrings =
+                Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon");
+        // myStrings.subscribe(s -> System.out.println(s));
+
+        Observable<Long> secondIntervals = Observable.interval(1, TimeUnit.SECONDS);
+        secondIntervals
+                .subscribe(aLong -> System.out.println(aLong));
+
+        /* Hold main thread for 5 seconds
+         * So, observable above has chance to fire */
+        sleep(5000);
+
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
